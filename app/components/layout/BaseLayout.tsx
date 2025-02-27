ce891d14539e41edd11275accafbdaaf7018c975
Sudhanshu <sudhanshu9252@gmail.com>
2025-02-27 13:56:17 +0530

    Update BaseLayout to use the ThemeProvider for reliable dark mode

--- app/components/layout/BaseLayout.tsx
+++ app/components/layout/BaseLayout.tsx
@@ -3,6 +3,7 @@
 import Header from './Header';
 import Footer from './Footer';
 import BackToTop from '../shared/BackToTop';
+import { ThemeProvider } from '../../context/ThemeProvider';
 
 interface BaseLayoutProps {
   children: React.ReactNode;
@@ -10,11 +11,13 @@ interface BaseLayoutProps {
 
 export default function BaseLayout({ children }: BaseLayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-grow pt-16">{children}</main>
-      <Footer />
-      <BackToTop />
-    </div>
+    <ThemeProvider defaultTheme="system" storageKey="indian-coffee-theme">
+      <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 transition-colors duration-200">
+        <Header />
+        <main className="flex-grow pt-16">{children}</main>
+        <Footer />
+        <BackToTop />
+      </div>
+    </ThemeProvider>
   );
 }
